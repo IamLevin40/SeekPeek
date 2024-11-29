bf7e021f1eb43f0419e5c69be77c2278d343e4c2
IamLevin40 <levincabales40@gmail.com>
2024-11-29 19:47:28 +0800

    Improved user interface with saved location

--- Assets/Scripts/GPSLocationManager.cs
+++ Assets/Scripts/GPSLocationManager.cs
@@ -15,37 +15,89 @@ public class GPSLocationManager : MonoBehaviour
     public string thingSpeakChannelId = "2729123";
     public string thingSpeakReadAPIKey = "FI6RJNDMMI3R2PX8";
     public string mapboxAccessToken = "pk.eyJ1IjoiaWFtbGV2aW4iLCJhIjoiY20zNTVneHZxMDE0NDJuc2FuazBsMXNqcCJ9.ufMmWdL6ivNgDSKCMxb5iw";
-    private double latitude;
-    private double longitude;
+    private DateTime currentDateTime;
+    private double currentLatitude;
+    private double currentLongitude;
+    private double currentAltitude;
     private int zoomLevel = 15;
     private int mapWidth = 1000;
     private int mapHeight = 1000;
+    private bool isNotifyToggleOn = false;
 
-    public Text latitudeText;
-    public Text longitudeText;
+    public Button informationTabButton;
+    public Button settingsTabButton;
+    public GameObject informationTab;
+    public GameObject settingsTab;
+    public Text currentDateTimeText;
+    public Text currentLatitudeText;
+    public Text currentLongitudeText;
+    public Text currentAltitudeText;
+    public Text savedDateTimeText;
+    public Text savedLatitudeText;
+    public Text savedLongitudeText;
+    public Text savedAltitudeText;
     public RawImage mapImage;
-    public Toggle locationToggle; // Reference to the toggle UI
-    private bool isToggleOn = false;
+    public Toggle notifyToggle;
+    public Button saveLocationButton;
+    public Slider thresholdSlider;
+    public InputField thresholdInputField;
+
+    private double threshold = 0.001;
+    private double previousThreshold;
 
     private const string POST_NOTIFICATIONS_PERMISSION = "android.permission.POST_NOTIFICATIONS";
 
 
     private void Start()
     {
+        OnTabPressed("information_tab");
+        
         InitializeNotifications();
         RequestNotificationPermission();
-        locationToggle.onValueChanged.AddListener(OnToggleChanged);
-        StartCoroutine(GetGPSData());
         StartForegroundService();
+
+        PrepareListeners();
+        StartCoroutine(GetGPSData());
     }
 
-    private void OnToggleChanged(bool value)
+    private void PrepareListeners()
     {
-        isToggleOn = value;
-        Debug.Log($"Toggle changed: {isToggleOn}");
+        notifyToggle.onValueChanged.AddListener(OnNotifyToggled);
+        informationTabButton.onClick.AddListener(() => OnTabPressed("information_tab"));
+        settingsTabButton.onClick.AddListener(() => OnTabPressed("settings_tab"));
+        
+        thresholdSlider.onValueChanged.AddListener(value => UpdateThresholdFromSlider(value));
+        thresholdInputField.onEndEdit.AddListener(value => UpdateThresholdFromInput(value));
+        previousThreshold = threshold;
+        UpdateThresholdUI();
 
-        if (isToggleOn)
-            SaveLocationToFile(latitude, longitude);
+        Vector3d? savedLocation = LoadLocationFromFile();
+        if (savedLocation.HasValue)
+        {
+            DisplaySavedLocation(savedLocation.Value.dateTime, savedLocation.Value.latitude, savedLocation.Value.longitude, savedLocation.Value.altitude);
+        }
+    }
+
+    private void OnNotifyToggled(bool value)
+    {
+        isNotifyToggleOn = value;
+        Debug.Log($"Notify toggle changed: {isNotifyToggleOn}");
+    }
+
+    private void OnTabPressed(string tab)
+    {
+        informationTab.SetActive(false);
+        settingsTab.SetActive(false);
+
+        switch (tab)
+        {
+            case "information_tab":
+                informationTab.SetActive(true);
+                break;
+            case "settings_tab":
+                settingsTab.SetActive(true);
+                break;
+        }
     }
 
     private IEnumerator GetGPSData()
@@ -66,23 +118,33 @@ public class GPSLocationManager : MonoBehaviour
             {
                 string jsonText = www.downloadHandler.text;
                 ThingSpeakData data = JsonUtility.FromJson<ThingSpeakData>(jsonText);
-                latitude = double.Parse(data.channel.latitude, System.Globalization.CultureInfo.InvariantCulture);
-                longitude = double.Parse(data.channel.longitude, System.Globalization.CultureInfo.InvariantCulture);
-
-                latitudeText.text = $"{latitude.ToString("f6")}°";
-                longitudeText.text = $"{longitude.ToString("f6")}°";
-
-                // Save location if toggle is on
-                if (isToggleOn)
+                
+                if (data.feeds.Length > 0)
                 {
-                    Vector2d? savedLocation = LoadLocationFromFile();
-                    if (savedLocation.HasValue)
+                    currentDateTime = DateTime.Parse(data.feeds[0].created_at, null, System.Globalization.DateTimeStyles.RoundtripKind);
+                    currentDateTime = currentDateTime.AddHours(-4);
+
+                    currentLatitude = double.Parse(data.feeds[0].field1, System.Globalization.CultureInfo.InvariantCulture);
+                    currentLongitude = double.Parse(data.feeds[0].field2, System.Globalization.CultureInfo.InvariantCulture);
+                    currentAltitude = double.Parse(data.feeds[0].field3, System.Globalization.CultureInfo.InvariantCulture);
+                    saveLocationButton.onClick.AddListener(() => SaveLocationToFile(currentDateTime, currentLatitude, currentLongitude, currentAltitude));
+
+                    currentDateTimeText.text = FormatTimestamp(currentDateTime);
+                    currentLatitudeText.text = $"{currentLatitude.ToString("f6")}°";
+                    currentLongitudeText.text = $"{currentLongitude.ToString("f6")}°";
+                    currentAltitudeText.text = $"{currentAltitude.ToString("f2")}m";
+
+                    if (isNotifyToggleOn)
                     {
-                        double distance = HaversineDistance(savedLocation.Value.x, savedLocation.Value.y, latitude, longitude);
-                        Debug.Log($"Saved: {savedLocation.Value.x}, {savedLocation.Value.y}... Current: {latitude}, {longitude}");
-                        if (distance > 0.000000001) // Use an appropriate threshold for the distance
+                        Vector3d? savedLocation = LoadLocationFromFile();
+                        if (savedLocation.HasValue)
                         {
-                            SendNotification("Warning", "Item is being moved by unknown!");
+                            double distance = HaversineDistance(savedLocation.Value.latitude, savedLocation.Value.longitude, currentLatitude, currentLongitude);
+                            Debug.Log($"Saved: {savedLocation.Value.latitude}, {savedLocation.Value.longitude}... Current: {currentLatitude}, {currentLongitude}");
+                            if (distance > threshold)
+                            {
+                                SendNotification("Warning", "Item is being moved by unknown!");
+                            }
                         }
                     }
                 }
@@ -111,8 +173,8 @@ public class GPSLocationManager : MonoBehaviour
 
     private IEnumerator GetMapImage()
     {
-        // Construct the Mapbox URL with the current latitude and longitude
-        string url = $"https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-l-marker+ff0000({longitude},{latitude})/{longitude},{latitude},{zoomLevel},0/{mapWidth}x{mapHeight}?access_token={mapboxAccessToken}";
+        // Construct the Mapbox URL with the current currentLatitude and currentLongitude
+        string url = $"https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-l-marker+ff0000({currentLongitude},{currentLatitude})/{currentLongitude},{currentLatitude},{zoomLevel},0/{mapWidth}x{mapHeight}?access_token={mapboxAccessToken}";
         Debug.Log(url);
         UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
         yield return request.SendWebRequest();
@@ -130,30 +192,102 @@ public class GPSLocationManager : MonoBehaviour
         }
     }
 
-    private void SaveLocationToFile(double latitude, double longitude)
+    private string FormatTimestamp(DateTime createdAt)
+    {
+        string formattedTimestamp = createdAt.ToString("yyyy-MM-dd | HH:mm:ss");
+        Debug.Log($"Formatted Timestamp: {formattedTimestamp}");
+        return formattedTimestamp;
+    }
+
+    private void DisplaySavedLocation(DateTime createdAt, double latitude, double longitude, double altitude)
+    {
+        savedDateTimeText.text = FormatTimestamp(createdAt);
+        savedLatitudeText.text = $"{latitude.ToString("f6")}°";
+        savedLongitudeText.text = $"{longitude.ToString("f6")}°";
+        savedAltitudeText.text = $"{altitude.ToString("f2")}m";
+    }
+
+    private void SaveLocationToFile(DateTime createdAt, double latitude, double longitude, double altitude)
     {
         string filePath = Application.persistentDataPath + "/location.dat";
-        string data = $"{latitude.ToString("R", System.Globalization.CultureInfo.InvariantCulture)},{longitude.ToString("R", System.Globalization.CultureInfo.InvariantCulture)}";
+        string data = $"{createdAt},{latitude.ToString("R", System.Globalization.CultureInfo.InvariantCulture)},{longitude.ToString("R", System.Globalization.CultureInfo.InvariantCulture)},{altitude.ToString("R", System.Globalization.CultureInfo.InvariantCulture)}";
         System.IO.File.WriteAllText(filePath, data);
+        
+        DisplaySavedLocation(createdAt, latitude, longitude, altitude);
         Debug.Log($"Saved location: {data}");
     }
 
-    private Vector2d? LoadLocationFromFile()
+    private Vector3d? LoadLocationFromFile()
     {
         string filePath = Application.persistentDataPath + "/location.dat";
         if (System.IO.File.Exists(filePath))
         {
             string data = System.IO.File.ReadAllText(filePath);
             string[] parts = data.Split(',');
-            if (parts.Length == 2 &&
-                double.TryParse(parts[0], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double savedLatitude) &&
-                double.TryParse(parts[1], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double savedLongitude))
+
+            if (parts.Length == 4 &&
+                DateTime.TryParse(parts[0], null, System.Globalization.DateTimeStyles.RoundtripKind, out DateTime savedDateTime) &&
+                double.TryParse(parts[1], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double savedLatitude) &&
+                double.TryParse(parts[2], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double savedLongitude) &&
+                double.TryParse(parts[3], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double savedAltitude))
             {
-                Debug.Log($"Loaded: {savedLatitude}, {savedLongitude}");
-                return new Vector2d(savedLatitude, savedLongitude);
+                Debug.Log($"Loaded: {savedDateTime}, {savedLatitude}, {savedLongitude}, {savedAltitude}");
+                return new Vector3d(savedDateTime, savedLatitude, savedLongitude, savedAltitude);
             }
         }
         return null;
+    }
+
+    private void UpdateThresholdFromSlider(float sliderValue)
+    {
+        // Map slider value to appropriate range and increment
+        if (sliderValue < 0.25f)
+            threshold = 0.00001 + sliderValue / 0.25 * (0.0001 - 0.00001);
+        else if (sliderValue < 0.5f)
+            threshold = 0.0001 + (sliderValue - 0.25f) / 0.25 * (0.001 - 0.0001);
+        else if (sliderValue < 0.75f)
+            threshold = 0.001 + (sliderValue - 0.5f) / 0.25 * (0.01 - 0.001);
+        else
+            threshold = 0.01 + (sliderValue - 0.75f) / 0.25 * (0.1 - 0.01);
+
+        UpdateThresholdInputField();
+    }
+
+    private void UpdateThresholdFromInput(string inputValue)
+    {
+        if (double.TryParse(inputValue, out double inputThreshold) && inputThreshold >= 0.00001 && inputThreshold <= 0.1)
+        {
+            threshold = inputThreshold;
+            UpdateThresholdSlider();
+            previousThreshold = threshold;
+        }
+        else
+        {
+            thresholdInputField.text = previousThreshold.ToString("0.#####");
+        }
+    }
+
+    private void UpdateThresholdUI()
+    {
+        UpdateThresholdSlider();
+        UpdateThresholdInputField();
+    }
+
+    private void UpdateThresholdSlider()
+    {
+        if (threshold < 0.0001)
+            thresholdSlider.value = (float)((threshold - 0.00001) / (0.0001 - 0.00001) * 0.25);
+        else if (threshold < 0.001)
+            thresholdSlider.value = 0.25f + (float)((threshold - 0.0001) / (0.001 - 0.0001) * 0.25);
+        else if (threshold < 0.01)
+            thresholdSlider.value = 0.5f + (float)((threshold - 0.001) / (0.01 - 0.001) * 0.25);
+        else
+            thresholdSlider.value = 0.75f + (float)((threshold - 0.01) / (0.1 - 0.01) * 0.25);
+    }
+
+    private void UpdateThresholdInputField()
+    {
+        thresholdInputField.text = threshold.ToString("0.#####");
     }
 
     private void InitializeNotifications()
@@ -208,36 +342,39 @@ public class GPSLocationManager : MonoBehaviour
         }
         catch (Exception ex)
         {
-            Debug.LogError($"Failed to start foreground service: {ex.Message}");
+            Debug.LogWarning($"Failed to start foreground service: {ex.Message}");
         }
     #endif
     }
 
-
     [System.Serializable]
-    public class ChannelData
+    public class Feed
     {
-        public int id;
-        public string name;
-        public string latitude;
-        public string longitude;
+        public string created_at;
+        public string field1;
+        public string field2;
+        public string field3;
     }
 
     [System.Serializable]
     public class ThingSpeakData
     {
-        public ChannelData channel;
+        public Feed[] feeds;
     }
 }
 
-public struct Vector2d
+public struct Vector3d
 {
-    public double x;
-    public double y;
+    public DateTime dateTime;
+    public double latitude;
+    public double longitude;
+    public double altitude;
 
-    public Vector2d(double x, double y)
+    public Vector3d(DateTime dateTime, double latitude, double longitude, double altitude)
     {
-        this.x = x;
-        this.y = y;
+        this.dateTime = dateTime;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.altitude = altitude;
     }
 }
